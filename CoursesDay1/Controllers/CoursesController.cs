479d5bb83dde698a439bdefcecebf51ff90bb08d
Azza <azzagamel11@gmail.com>
2024-04-17 18:14:49 +0200

    init

---
+++ CoursesDay1/Controllers/CoursesController.cs
@@ -0,0 +1,76 @@
+﻿using CoursesDay1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursesDay1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        APIContext db;
+        public CoursesController(APIContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public ActionResult GetAllCourses()
+        {
+            List<Course> courses = db.Courses.ToList();
+            if (courses.Count == 0) return NotFound(); //404 
+            else return Ok(courses); //200 -> succeeded+OK
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult getById(int id)
+        {
+            var course = db.Courses.Find(id);
+            if (course == null) return NotFound(); //404 
+            else return Ok(course); //200 -> succeeded+OK
+        }
+
+        [HttpGet("/api/CoursesByName/{name}")]  //change routing because it will conflict with getById
+        public ActionResult couseByName(string name)
+        {
+            var course = db.Courses.FirstOrDefault(x => x.CrsName == name);
+            if (course == null) return NotFound(); //404 
+            else return Ok(course); //200 -> succeeded+OK
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult deleteCourse(int id)
+        {
+            Course c = db.Courses.Find(id);
+            if (c == null) return NotFound();  //404 
+            else
+            {
+                db.Courses.Remove(c);
+                db.SaveChanges();
+                return Ok(c);   //200 -> succeeded+OK
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Course c)
+        {
+            if (c == null) return BadRequest();  //400
+            else
+            {
+                db.Courses.Add(c);
+                db.SaveChanges();
+                return CreatedAtAction("getById", new { id = c.Id }, c);  //201 -> succeeded + create (call action to display the latest added course)
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult EditCourse(Course c, int id)
+        {
+            if (c.Id != id) return BadRequest();  //400
+            if (c == null) return NotFound();  //404
+            db.Entry(c).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return NoContent();  //204 -> succeeded + no Content
+        }
+
+    }
+}
---
+++ CoursesDay1/Program.cs
@@ -0,0 +1,41 @@
+
+using CoursesDay1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesDay1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<APIContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("APICon")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
